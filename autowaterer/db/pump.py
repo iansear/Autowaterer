0eb5e9bc37237be9043a9f782d8a79471fc5c63b
Ian Sear <iansear@Ians-MacBook-Pro.local>
2026-09-08 20:49:51 -0500

    Fix import

--- autowaterer/db/pump.py
+++ autowaterer/db/pump.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import DateTime, Integer, String, Text
+from sqlalchemy import DateTime, Integer, String, Text, Float
 from sqlalchemy.orm import Mapped, mapped_column
 from . import db
 
